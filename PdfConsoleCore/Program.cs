f2d93fb9200fde298e559b599ab4319549a693ef
Jackson Bowen <jacksonabowen@outlook.com>
2021-06-23 23:58:34 -0400

    added image and address

--- PdfConsoleCore/Program.cs
+++ PdfConsoleCore/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Drawing;
 using System.IO;
 using PdfSharpCore;
 using PdfSharpCore.Drawing;
@@ -11,86 +12,22 @@ namespace PdfConsoleCore
 {
 	class Program
 	{
-		//static void Main(string[] args)
-		//{
-		//	GlobalFontSettings.FontResolver = new FontResolver();
-
-		//	var document = new PdfDocument();
-		//	var page = document.AddPage();
-		//	var gfx = XGraphics.FromPdfPage(page);
-		//	var font = new XFont("OpenSans", 20, XFontStyle.Bold);
-		//	string currentTimeString = DateTime.UtcNow.ToString("yyyyMMddHHmmss");
-
-		//	gfx.DrawString(
-		//		"Hello World!",
-		//		font,
-		//		XBrushes.Black,
-		//		new XRect(20, 20, page.Width, page.Height),
-		//		XStringFormats.Center);
-
-		//	string filename = "c:\\test\\testpdf.pdf";
-
-		//	document.Save(filename);
-
-		//}
-		public class LayoutHelper
-		{
-			private readonly PdfDocument _document;
-			private readonly XUnit _topPosition;
-			private readonly XUnit _bottomMargin;
-			private XUnit _currentPosition;
-
-			public LayoutHelper(PdfDocument document, XUnit topPosition, XUnit bottomMargin)
-			{
-				_document = document;
-				_topPosition = topPosition;
-				_bottomMargin = bottomMargin;
-				// Set a value outside the page - a new page will be created on the first request.
-				_currentPosition = bottomMargin + 10000;
-			}
-
-			public XUnit GetLinePosition(XUnit requestedHeight)
-			{
-				return GetLinePosition(requestedHeight, -1f);
-			}
-
-			public XUnit GetLinePosition(XUnit requestedHeight, XUnit requiredHeight)
-			{
-				XUnit required = requiredHeight == -1f ? requestedHeight : requiredHeight;
-				if (_currentPosition + required > _bottomMargin)
-					CreatePage();
-				XUnit result = _currentPosition;
-				_currentPosition += requestedHeight;
-
-				return result;
-			}
-
-			public XGraphics Gfx { get; private set; }
-
-			public PdfPage Page { get; private set; }
-
-			void CreatePage()
-			{
-				Page = _document.AddPage();
-				Page.Size = PageSize.A4;
-				Gfx = XGraphics.FromPdfPage(Page);
-				_currentPosition = _topPosition;
-			}
-		}
-
 		static void Main(string[] args)
 		{
-			PdfDocument document = new PdfDocument();
+			GlobalFontSettings.FontResolver = new FontResolver();
 
-			// Sample uses DIN A4, page height is 29.7 cm. We use margins of 2.5 cm.
-			LayoutHelper helper = new LayoutHelper(
-				document,
-				XUnit.FromCentimeter(2.5),
-				XUnit.FromCentimeter(29.7 - 2.5));
-			XUnit left = XUnit.FromCentimeter(2.5);
+			//pdf settings
+			var document = new PdfDocument();
+			var page = document.AddPage();
+			var gfx = XGraphics.FromPdfPage(page);
 
-			// Random generator with seed value, so created document will always be the same.
-			Random rand = new Random(42);
+			//files
+			string currentTimeString = DateTime.UtcNow.ToString("yyyyMMddHHmmss");
+			string filename = "c:\\test\\testpdf.pdf";
+			string image = "C:\\Users\\jacks\\Documents\\Visual Studio 2019\\Projects\\PdfConsole\\PdfConsoleCore\\Assets\\aca.png";
+
+			//fonts
+			var font = new XFont("OpenSans", 20, XFontStyle.Bold);
 
 			const int headerFontSize = 20;
 			const int normalFontSize = 10;
@@ -98,29 +35,82 @@ namespace PdfConsoleCore
 			XFont fontHeader = new XFont("Verdana", headerFontSize, XFontStyle.BoldItalic);
 			XFont fontNormal = new XFont("Verdana", normalFontSize, XFontStyle.Regular);
 
-			const int totalLines = 666;
-			bool washeader = false;
+			string accountNumber = "1234567890";
+			string userName = "Test User";
+			string address = "123 Test";
+			string city = "Test";
+			string state = "TS";
+			string zip = "12345";
 
-			for (int line = 0; line < totalLines; ++line)
-			{
-				bool isHeader = line == 0 || !washeader && line < totalLines - 1 && rand.Next(15) == 0;
-				washeader = isHeader;
-				// We do not want a single header at the bottom of the page, so if we have a header we require space for header and a normal text line.
-				XUnit top = helper.GetLinePosition(
-					isHeader ? headerFontSize + 5 : normalFontSize + 2,
-					isHeader ? headerFontSize + 5 + normalFontSize : normalFontSize);
+			string addressFormatted =
+				$"User : {userName}" +
+				$"Address: {address}" +
+				$"City: {city}" +
+				$"State: {state}" +
+				$"Zip: {zip}";
 
-				helper.Gfx.DrawString(
-					isHeader
-						? "Sed massa libero, semper a nisi nec"
-						: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-					isHeader ? fontHeader : fontNormal,
-					XBrushes.Black, left, top,
-					XStringFormats.TopLeft);
-			}
+			//var sampleText =
+			//	"Facin exeraessisit la consenim iureet dignibh eu facilluptat vercil dunt autpat. " +
+			//	"Ecte magna faccum dolor sequisc iliquat, quat, quipiss equipit accummy niate magna " +
+			//	"facil iure eraesequis am velit, quat atis dolore dolent luptat nulla adio odipissectet " +
+			//	"lan venis do essequatio conulla facillandrem zzriusci bla ad minim inis nim velit eugait " +
+			//	"aut aut lor at ilit ut nulla ate te eugait alit augiamet ad magnim iurem il eu feuissi.\n" +
+			//	"Guer sequis duis eu feugait luptat lum adiamet, si tate dolore mod eu facidunt adignisl in " +
+			//	"henim dolorem nulla faccum vel inis dolutpatum iusto od min ex euis adio exer sed del " +
+			//	"dolor ing enit veniamcon vullutat praestrud molenis ciduisim doloborem ipit nulla consequisi.\n" +
+			//	"Nos adit pratetu eriurem delestie del ut lumsandreet nis exerilisit wis nos alit venit praestrud " +
+			//	"dolor sum volore facidui blaor erillaortis ad ea augue corem dunt nis  iustinciduis euisi.\n" +
+			//	"Ut ulputate volore min ut nulpute dolobor sequism olorperilit autatie modit wisl illuptat dolore " +
+			//	"min ut in ute doloboreet ip ex et am dunt at.";
+
+			gfx.DrawImage(XImage.FromFile(image), 30, 15);
+			
+			gfx.DrawString(
+				$"Account : {accountNumber}",
+				fontHeader,
+				XBrushes.Black,
+				new XRect(30, 150, page.Width, page.Height),
+				XStringFormats.TopLeft);
+
+
+			gfx.DrawString($"User      : {userName}",
+				fontNormal,
+				XBrushes.Black,
+				new XRect(30, 200, page.Width, page.Height),
+				XStringFormats.TopLeft);
+
+			gfx.DrawString($"Address : {address}",
+				fontNormal,
+				XBrushes.Black,
+				new XRect(30, 210, page.Width, page.Height),
+				XStringFormats.TopLeft);
+
+			gfx.DrawString($"City       : {city}",
+				fontNormal,
+				XBrushes.Black,
+				new XRect(30, 220, page.Width, page.Height),
+				XStringFormats.TopLeft);
+
+			gfx.DrawString($"State     : {state}",
+				fontNormal,
+				XBrushes.Black,
+				new XRect(30, 230, page.Width, page.Height),
+				XStringFormats.TopLeft);
+
+			gfx.DrawString($"Zip        : {zip}",
+				fontNormal,
+				XBrushes.Black,
+				new XRect(30, 240, page.Width, page.Height),
+				XStringFormats.TopLeft);
+
+			//gfx.DrawString(sampleText,
+			//	fontNormal,
+			//	XBrushes.Black,
+			//	new XRect(30, 250, page.Width, page.Height),
+			//	XStringFormats.TopLeft);
 
 			// Save the document... 
-			const string filename = "c:\\test\\MultiplePages.pdf";
+
 			document.Save(filename);
 			// ...and start a viewer.
 			//}
